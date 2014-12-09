24534588e728d7e7ede11f3795a0de8340197ffc
Charlotte Holmen <holmen.charlotte@gmail.com>
2014-12-09 22:28:10 +0100

    swipeshow indentation

--- ui/js/swipeshow.js
+++ ui/js/swipeshow.js
@@ -1,18 +1,18 @@
 /*
- * Swipeshow.js - Slideshow with swipe support
- *
- * Copyright (c) Charlotte Holmen
- *
- * www.howler.se
- *
- * Version:  1.1
- *
- */
+* Swipeshow.js - Slideshow with swipe support
+*
+* Copyright (c) Charlotte Holmen
+*
+* www.howler.se
+*
+* Version:  1.1
+*
+*/
 
 
 
 define('swipeshow', ['jquery'], 
-	function ($) {
+function ($) {
 
 	'use strict';
 
@@ -22,225 +22,216 @@ define('swipeshow', ['jquery'],
 
 	Swipeshow.prototype = {
 
-		defaults : {
-			imagesArray : [],
-			swipeshowLeftOffset : 0,
-			swipeDelay : 30,
-			auto : true,
-			enableArrows : false
-		},
+	defaults : {
+		imagesArray : [],
+		swipeshowLeftOffset : 0,
+		swipeDelay : 30,
+		auto : true,
+		enableArrows : false
+	},
 
-		init : function () {
+	init : function () {
 
-			var self = this,
-				images = $('img', self.$element);
+		var self = this,
+		images = $('img', self.$element);
 
-			if ($(window).width() > 479) {
-				self.defaults.enableArrows = true;
-			} else if ($(window).width() > 1023) {
-				self.defaults.swipeDelay = 300;
+		if ($(window).width() > 479) {
+			self.defaults.enableArrows = true;
+		} else if ($(window).width() > 1023) {
+			self.defaults.swipeDelay = 300;
+		}
+
+		self.settings = $.extend({}, self.options, self.defaults);
+
+		if (self.settings.enableArrows) {
+			self.settings.$arrowBack.show();
+			self.settings.$arrowForward.show();
+		}
+
+		//Save images in array
+		images.each(function() {
+			self.settings.imagesArray.push($(this));
+		});
+
+		//Set $holder width
+		self.settings.$holder.css('width', (self.imagesArray.length * 100) + '%');
+
+		self.resetImages();
+	},
+
+	swipeshowWidth : function () {
+		return this.settings.$element[0].scrollWidth;
+	},
+
+	imageWidth : function () {
+		return Math.floor(this.settings.$element[0].scrollWidth / this.imagesArray.length);
+	},
+
+	resetImages : function (direction) {	
+
+		var self = this,
+		i,
+		images,
+		htmlString = '';
+
+		// Current image is always number 2 in imagesArray. 
+		// Replace html string with current array
+		if (direction === 'forward') {
+			self.settings.imagesArray.push(self.settings.imagesArray[0]);
+			self.settings.imagesArray.shift();
+		} else {
+			self.settings.imagesArray.unshift(self.settings.imagesArray[self.settings.imagesArray.length - 1]);
+			self.settings.imagesArray.pop();
+		}
+
+		for (i = 0; i < self.settings.imagesArray.length; i++) {
+			htmlString = htmlString + self.settings.imagesArray[i][0].outerHTML;
+		}
+
+		self.settings.$holder.html(htmlString);
+
+		images = $('img', self.settings.$holder);
+		images.css('width', (100 / self.settings.imagesArray.length) + '%');
+		self.settings.$element.scrollLeft(self.settings.imageWidth());
+		self.settings.swipeshowLeftOffset = self.settings.imageWidth();
+
+		self.enableSwipes();
+	},
+
+	enableSwipes : function () {
+
+		var self = this;
+
+		self.settings.$element.bind("scroll", function(){
+			if (!self.settings.$element.hasClass('active')) {
+				self.settings.manuelSwipe();
 			}
+		});
 
-			self.settings = $.extend({}, self.options, self.defaults);
-
-			if (self.settings.enableArrows) {
-				self.settings.$arrowBack.show();
-				self.settings.$arrowForward.show();
-			}
-			
-			//Save images in array
-			images.each(function() {
-				self.settings.imagesArray.push($(this));
-			});
-
-			//Set $holder width
-			self.settings.$holder.css('width', (self.imagesArray.length * 100) + '%');
-
-			self.resetImages();
-		},
-
-		swipeshowWidth : function () {
-
-			var self = this;
-
-			return self.settings.$element[0].scrollWidth;
-		},
-
-		imageWidth : function () {
-
-			var self = this;
-
-			return Math.floor(self.settings.$element[0].scrollWidth / self.imagesArray.length);
-		},
-
-		resetImages : function (direction) {	
-
-			var self = this,
-				i,
-    			images,
-    			htmlString = '';
-    			
-			// Current image is always number 2 in imagesArray. 
-			// Replace html string with current array
-			if (direction === 'forward') {
-				self.settings.imagesArray.push(self.settings.imagesArray[0]);
-				self.settings.imagesArray.shift();
-			} else {
-				self.settings.imagesArray.unshift(self.settings.imagesArray[self.settings.imagesArray.length - 1]);
-				self.settings.imagesArray.pop();
-			}
-			
-			for (i = 0; i < self.settings.imagesArray.length; i++) {
-				htmlString = htmlString + self.settings.imagesArray[i][0].outerHTML;
-			}
-
-			self.settings.$holder.html(htmlString);
-
-			images = $('img', self.settings.$holder);
-			images.css('width', (100 / self.settings.imagesArray.length) + '%');
-			self.settings.$element.scrollLeft(self.settings.imageWidth());
-			self.settings.swipeshowLeftOffset = self.settings.imageWidth();
-
-			self.enableSwipes();
-    	},
-
-		enableSwipes : function () {
-
-			var self = this;
-
-			self.settings.$element.bind("scroll", function(){
+		if (self.settings.enableArrows) {
+			self.settings.$arrowBack.bind('click', function (e) {
+				e.preventDefault();
 				if (!self.settings.$element.hasClass('active')) {
-					self.settings.manuelSwipe();
+					self.settings.auto = false;
+					self.backAnimation();
 				}
 			});
-			
-			if (self.settings.enableArrows) {
-				self.settings.$arrowBack.bind('click', function (e) {
-					e.preventDefault();
-					if (!self.settings.$element.hasClass('active')) {
-						self.settings.auto = false;
-						self.backAnimation();
-					}
-				});
 
-				self.settings.$arrowForward.bind('click', function (e) {
-					e.preventDefault();
-					if (!self.settings.$element.hasClass('active')) {
-						self.settings.auto = false;
-						self.forwardAnimation();
-					}
-				});
-			}
-
-			self.settings.disableAutoSwipe();
-
-			if (self.settings.auto === true) {
-				self.autoSwipe();
-			}
-
-			self.settings.$element.removeClass('active');
-		},
-
-		disableSwipes : function () {
-
-			var self = this;
-
-			self.settings.$element.addClass('active');
-    	}, 
-
-    	autoSwipe : function () {
-
-    		var self = this;
-
-    		var interval = setInterval(function(){
-    			self.forwardAnimation();
-    		}, 5000);
-
-    		self.settings.interval = interval;
-    	},
-
-    	disableAutoSwipe : function () {
-
-    		var self = this;
-
-    		clearInterval(self.settings.interval);
-    	},
-
-		manuelSwipe : function () {
-
-			var self = this;
-
-			if (self.settings.swipeshowLeftOffset !== self.settings.$element[0].scrollLeft) {
-				self.settings.auto = false;
-			}
-
-			self.preventScrollTooFar();
-
-	        //If swipe half the picture - Wait until swipe stopped, then do animation
-			clearTimeout($.data(swipeshow, 'scrollTimer'));
-			$.data(swipeshow, 'scrollTimer', setTimeout(function() {
-
-				
-				if (self.settings.swipeshowLeftOffset < self.settings.$element[0].scrollLeft) {
-					self.disableSwipes();
+			self.settings.$arrowForward.bind('click', function (e) {
+				e.preventDefault();
+				if (!self.settings.$element.hasClass('active')) {
+					self.settings.auto = false;
 					self.forwardAnimation();
-				} else if (self.settings.swipeshowLeftOffset > self.settings.$element[0].scrollLeft) {
-					self.disableSwipes();
-					self.backAnimation();
-				}			
+				}
+			});
+		}
 
-			}, self.settings.swipeDelay));
-		},
+		self.settings.disableAutoSwipe();
 
-		preventScrollTooFar : function () {
+		if (self.settings.auto === true) {
+			self.autoSwipe();
+		}
 
-			var self = this;
+		self.settings.$element.removeClass('active');
+	},
 
-			//If swipe the whole picture - STOP
+	disableSwipes : function () {
+
+		var self = this;
+
+		self.settings.$element.addClass('active');
+	}, 
+
+	autoSwipe : function () {
+
+		var self = this;
+
+		var interval = setInterval(function(){
+			self.forwardAnimation();
+		}, 5000);
+
+		self.settings.interval = interval;
+	},
+
+	disableAutoSwipe : function () {
+
+		var self = this;
+
+		clearInterval(self.settings.interval);
+	},
+
+	manuelSwipe : function () {
+
+		var self = this;
+
+		if (self.settings.swipeshowLeftOffset !== self.settings.$element[0].scrollLeft) {
+			self.settings.auto = false;
+		}
+
+		self.preventScrollTooFar();
+
+		//If swipe half the picture - Wait until swipe stopped, then do animation
+		clearTimeout($.data(swipeshow, 'scrollTimer'));
+		
+		$.data(swipeshow, 'scrollTimer', setTimeout(function() {
 			if (self.settings.swipeshowLeftOffset < self.settings.$element[0].scrollLeft) {
-	        	if (self.settings.$element[0].scrollLeft >= (self.imageWidth() * 2)) {
-	        		self.settings.$element[0].scrollLeft = self.imageWidth() * 2;
-	        		self.resetImages('forward');
-	        	}
-	        } else if (self.settings.swipeshowLeftOffset > self.settings.$element[0].scrollLeft) {
-	        	if (self.settings.$element[0].scrollLeft === 0) {
-	        		self.settings.$element[0].scrollLeft = 0;
-	        		self.resetImages('back');
-	        	}
-	        }
-		},
+				self.disableSwipes();
+				self.forwardAnimation();
+			} else if (self.settings.swipeshowLeftOffset > self.settings.$element[0].scrollLeft) {
+				self.disableSwipes();
+				self.backAnimation();
+			}			
+		}, self.settings.swipeDelay));
+	},
 
-		forwardAnimation : function () {
+	preventScrollTooFar : function () {
 
-			var self = this;
+	var self = this;
 
-    		self.disableSwipes();
-			self.animation('forward', self.imageWidth() * 2);
-    	},
+		//If swipe the whole picture - STOP
+		if (self.settings.swipeshowLeftOffset < self.settings.$element[0].scrollLeft) {
+			if (self.settings.$element[0].scrollLeft >= (self.imageWidth() * 2)) {
+				self.settings.$element[0].scrollLeft = self.imageWidth() * 2;
+				self.resetImages('forward');
+			}
+		} else if (self.settings.swipeshowLeftOffset > self.settings.$element[0].scrollLeft) {
+			if (self.settings.$element[0].scrollLeft === 0) {
+				self.settings.$element[0].scrollLeft = 0;
+				self.resetImages('back');
+			}
+		}
+	},
 
-    	backAnimation : function () {
+	forwardAnimation : function () {
 
-    		var self = this;
+		var self = this;
 
-    		self.disableSwipes();
-			self.animation('back', 0);
-    	},
+		self.disableSwipes();
+		self.animation('forward', self.imageWidth() * 2);
+	},
 
-    	animation : function (direction, goToOffset) {
+	backAnimation : function () {
 
-    		var self = this;
+		var self = this;
 
-    		self.settings.$element.animate({scrollLeft: goToOffset}, 400, function() {
-				clearTimeout($.data(self, 'scrollTimer'));
-    			$.data(self, 'scrollTimer', setTimeout(function() {
-					self.resetImages(direction);
+		self.disableSwipes();
+		self.animation('back', 0);
+	},
+
+	animation : function (direction, goToOffset) {
+
+		var self = this;
+
+		self.settings.$element.animate({scrollLeft: goToOffset}, 400, function() {
+			clearTimeout($.data(self, 'scrollTimer'));
+			$.data(self, 'scrollTimer', setTimeout(function() {
+				self.resetImages(direction);
 				}, 10));
 			});
-    	}
-
+		}
 	}
 
 
-    return Swipeshow;
+	return Swipeshow;
 
 });
